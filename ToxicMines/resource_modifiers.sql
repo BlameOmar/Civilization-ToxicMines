db51ac034f0413c82d33154979da099ee3ae7d43
Omar Stefan Evans <omar@omarevans.com>
2018-11-12 20:25:41 -0500

    Forgot to add these files in the last commit.

--- ToxicMines/resource_modifiers.sql
+++ ToxicMines/resource_modifiers.sql
@@ -1,11 +1,19 @@
 UPDATE ModifierArguments
-SET Value = (SELECT YieldChange FROM Resource_YieldChanges WHERE YieldType == 'YIELD_PRODUCTION' AND ResourceType == 'RESOURCE_COAL')
+SET Value = (SELECT YieldChange FROM Resource_YieldChanges WHERE YieldType == 'YIELD_SCIENCE' AND ResourceType == 'RESOURCE_IRON')
+WHERE ModifierId == 'TOXIC_MINES__IRON_MINE_BONUS_PRODUCTION' AND Name == 'Amount';
+
+UPDATE ModifierArguments
+SET Value = (SELECT YieldChange FROM Resource_YieldChanges WHERE YieldType == 'YIELD_SCIENCE' AND ResourceType == 'RESOURCE_ALUMINUM')
+WHERE ModifierId == 'TOXIC_MINES__ALUMINUM_MINE_BONUS_PRODUCTION' AND Name == 'Amount';
+
+UPDATE ModifierArguments
+SET Value = (SELECT YieldChange FROM Resource_YieldChanges WHERE YieldType == 'YIELD_SCIENCE' AND ResourceType == 'RESOURCE_COAL')
 WHERE ModifierId == 'TOXIC_MINES__COAL_MINE_COMPENSATED_PRODUCTION' AND Name == 'Amount';
 
 UPDATE ModifierArguments
-SET Value = (SELECT YieldChange FROM Resource_YieldChanges WHERE YieldType == 'YIELD_PRODUCTION' AND ResourceType == 'RESOURCE_URANIUM')
+SET Value = (SELECT YieldChange FROM Resource_YieldChanges WHERE YieldType == 'YIELD_SCIENCE' AND ResourceType == 'RESOURCE_URANIUM')
 WHERE ModifierId == 'TOXIC_MINES__URANIUM_MINE_COMPENSATED_PRODUCTION' AND Name == 'Amount';
 
 UPDATE ModifierArguments
-SET Value = (SELECT YieldChange FROM Resource_YieldChanges WHERE YieldType == 'YIELD_PRODUCTION' AND ResourceType == 'RESOURCE_OIL')
+SET Value = (SELECT YieldChange FROM Resource_YieldChanges WHERE YieldType == 'YIELD_SCIENCE' AND ResourceType == 'RESOURCE_OIL')
 WHERE ModifierId == 'TOXIC_MINES__EXPLOITED_OIL_COMPENSATED_PRODUCTION' AND Name == 'Amount';
